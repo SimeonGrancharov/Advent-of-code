eba09f90fa9810703286ec389b3f43540b2266ce
simeon.grancharov <simeon.grancharov@modeshift.com>
2024-12-05 09:31:40 +0200

    day 5 part 1

---
+++ 2024/src/day5/index.ts
@@ -0,0 +1,104 @@
+import { after, before } from 'node:test'
+import { loadDataFromFile } from '../utility'
+
+type DataT = {
+  beforeLookup: Record<string, string[]>
+  afterLookup: Record<string, string[]>
+
+  updates: string[][]
+}
+
+function parseInput(input: string[]) {
+  const result: DataT = {
+    afterLookup: {},
+    beforeLookup: {},
+
+    updates: [],
+  }
+
+  const indexOfDelimiter = input.indexOf('')
+
+  const instructions = input.slice(0, indexOfDelimiter)
+  const updates = input.slice(indexOfDelimiter + 1)
+
+  for (const instruction of instructions) {
+    const [left, right] = instruction.split('|')
+
+    if (!result.beforeLookup[left]) {
+      result.beforeLookup[left] = []
+    }
+
+    result.beforeLookup[left].push(right)
+
+    if (!result.afterLookup[right]) {
+      result.afterLookup[right] = []
+    }
+
+    result.afterLookup[right].push(left)
+  }
+
+  for (const update of updates) {
+    if (!update) {
+      continue
+    }
+
+    result.updates.push(update.split(','))
+  }
+
+  return result
+}
+
+function part1(input: string[]) {
+  const parsed = parseInput(input)
+
+  const validInputs: string[][] = []
+
+  for (const update of parsed.updates) {
+    let isValid = true
+
+    for (let index = 0; index <= update.length - 1; index++) {
+      const seq = update[index]
+      const beforeRule = parsed.beforeLookup[seq]
+
+      if (beforeRule) {
+        isValid = update.slice(index + 1).every((x) => beforeRule.includes(x))
+      }
+
+      if (isValid) {
+        const afterRule = parsed.afterLookup[seq]
+
+        if (afterRule) {
+          isValid = update.slice(0, index).every((x) => afterRule.includes(x))
+        }
+      }
+
+      if (!isValid) {
+        break
+      }
+    }
+
+    if (isValid) {
+      validInputs.push(update)
+    }
+  }
+
+  return validInputs.reduce((sum, update) => {
+    const middle = parseInt(update[Math.floor(update.length / 2)])
+
+    console.log(middle)
+
+    return sum + middle
+  }, 0)
+}
+
+function main() {
+  const data = loadDataFromFile(__dirname + '/data.txt', () => true)
+
+  console.log('============== Part 1 ================')
+  console.log(part1(data))
+  console.log('\n\n')
+  // console.log('============== Part 2 ================')
+  // console.log(part2(data.map((x) => x.split(''))))
+}
+
+main()
--- 2024/src/utility.ts
+++ 2024/src/utility.ts
@@ -1,5 +1,12 @@
 import fs from 'fs'
 
-export const loadDataFromFile = (filePath: string): string[] => {
-  return fs.readFileSync(filePath).toString().split('\n').filter(Boolean)
+export const loadDataFromFile = (
+  filePath: string,
+  filterFn?: (d: string) => boolean
+): string[] => {
+  return fs
+    .readFileSync(filePath)
+    .toString()
+    .split('\n')
+    .filter(filterFn ?? Boolean)
 }
