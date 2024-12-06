45d3aa603189c6a1ce874fc418af02a5e19bc3ce
simeon.grancharov <simeon.grancharov@modeshift.com>
2024-12-06 16:13:53 +0200

    Day 6 part 2 (whew)

--- 2024/src/day6/index.ts
+++ 2024/src/day6/index.ts
@@ -29,7 +29,7 @@ function move(
     const newPath = map[newPosition[0]]?.[newPosition[1]]
 
     // Obstacle is hit  couldn't move :/
-    if (newPath === '#') {
+    if (newPath === '#' || newPath === 'O') {
       return [row, col]
     }
 
@@ -40,7 +40,7 @@ function move(
     const newPosition: [number, number] = [row, col + 1]
     const newPath = map[newPosition[0]]?.[newPosition[1]]
     // Obstacle is hit  couldn't move :/
-    if (newPath === '#') {
+    if (newPath === '#' || newPath === 'O') {
       return [row, col]
     }
 
@@ -52,7 +52,7 @@ function move(
     const newPath = map[newPosition[0]]?.[newPosition[1]]
 
     // Obstacle is hit  couldn't move :/
-    if (newPath === '#') {
+    if (newPath === '#' || newPath === 'O') {
       return [row, col]
     }
 
@@ -63,7 +63,7 @@ function move(
     const newPath = map[newPosition[0]]?.[newPosition[1]]
 
     // Obstacle is hit  couldn't move :/
-    if (newPath === '#') {
+    if (newPath === '#' || newPath === 'O') {
       return [row, col]
     }
 
@@ -73,22 +73,20 @@ function move(
   return [row, col]
 }
 
-function part1(map: MapT): number {
-  let steps = 1
-
-  const currentPosition = [...getStartPosition(map)]
+function findPath(
+  map: MapT,
+  onStep: (coord: [number, number], direction: DirectionT) => boolean | void
+) {
+  const currentPosition: [number, number] = [...getStartPosition(map)]
   let direction: DirectionT = 'up'
-  // console.log(' starting on ', currentPosition)
-  map[currentPosition[0]][currentPosition[1]] = 'X'
 
-  let itt = 0
+  onStep(currentPosition, direction)
   while (
     currentPosition[0] >= 0 &&
     currentPosition[0] <= map.length - 1 &&
     currentPosition[1] >= 0 &&
     currentPosition[1] <= map[0].length - 1
   ) {
-    itt++
     const newPosition = move(
       map,
       currentPosition[0],
@@ -125,24 +123,61 @@ function part1(map: MapT): number {
     currentPosition[0] = newPosition[0]
     currentPosition[1] = newPosition[1]
 
-    if (['.', '^'].includes(map[currentPosition[0]]?.[currentPosition[1]])) {
-      map[currentPosition[0]][currentPosition[1]] = 'X'
+    const shouldBreak = onStep(currentPosition, direction)
 
-      steps++
+    if (shouldBreak) {
+      break
     }
   }
-  //
-  console.log(map.map((x) => x.join('')).join('\n'))
-
-  return steps
 }
 
-function getSymbolForDirection() {}
+function part1(map: MapT): number {
+  const uniqueSteps = new Set<string>()
 
-function part2(data: MapT): number {
-  let steps = 0
+  findPath(map, (coords) => {
+    uniqueSteps.add(`${coords[0]}-${coords[1]}`)
+  })
 
-  return steps
+  return uniqueSteps.size - 1
+}
+
+function part2(workData: MapT): number {
+  const path: Record<`${number}-${number}`, [number, number]> = {}
+
+  findPath(
+    workData.map((x) => [...x]),
+    (coords) => {
+      if (
+        coords[0] >= 0 &&
+        coords[0] < workData.length &&
+        coords[1] >= 0 &&
+        coords[1] < workData[0].length
+      ) {
+        path[`${coords[0]}-${coords[1]}`] = [coords[0], coords[1]]
+      }
+    }
+  )
+
+  let infiniteLoopsCount: number = 0
+
+  for (const point of Object.values(path)) {
+    const map = workData.map((x) => [...x])
+
+    map[point[0]][point[1]] = '#'
+
+    const visitedPoints: Record<`${number}-${number}-${DirectionT}`, true> = {}
+
+    findPath(map, (coords, direction) => {
+      if (visitedPoints[`${coords[0]}-${coords[1]}-${direction}`]) {
+        infiniteLoopsCount++
+        return true
+      } else {
+        visitedPoints[`${coords[0]}-${coords[1]}-${direction}`] = true
+      }
+    })
+  }
+
+  return infiniteLoopsCount
 }
 
 function main() {
